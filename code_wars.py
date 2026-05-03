7438c6bf92da35e1f7fe2aea66ec450449d158ad
Murod Akhmedov <python_webdev@outlook.com>
2026-05-03 18:54:39 +0100

    refactor(code_wars): solve code wars tasks

--- code_wars.py
+++ code_wars.py
@@ -9,10 +9,44 @@ print(sorted_text)  # Output: ['apple', 'Banana', 'Cherry', 'date']
 
 
 def remove_every_other(my_list: list) -> list:
-    return my_list[::2]
+    # return my_list[::2]
+    # return [my_list[i] for i in range(len(my_list)) if i % 2 == 0]
+    return [item for index, item in enumerate(my_list) if index % 2 == 0]
 
 
 # Example usage:
 input_list = [1, 2, 3, 4, 5, 6]
 result = remove_every_other(input_list)
 print(result)  # Output: [1, 3, 5]
+
+
+def count_positives_sum_negatives(lst: list[int]) -> list[int]:
+    if not lst:
+        return []
+    positive_count = sum(1 for number in lst if number > 0)
+    negative_sum = sum(number for number in lst if number < 0)
+    return [positive_count, negative_sum]
+
+
+# Example usage:
+numbers = [1, -2, 3, -4, 5]
+result = count_positives_sum_negatives(numbers)
+print(result)  # Output: [3, -9]
+
+
+def longest_common_prefix(strs: list[str]) -> str:
+    if not strs:
+        return ""
+    prefix = strs[0]
+    for s in strs[1:]:
+        while not s.startswith(prefix):
+            prefix = prefix[:-1]
+            if not prefix:
+                return ""
+    return prefix
+
+
+# Example usage:
+strs = ["flower", "flow", "flight"]
+result = longest_common_prefix(strs)
+print(result)  # Output: "fl"
